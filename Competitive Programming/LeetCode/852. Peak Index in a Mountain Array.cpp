f3202711ebed7059913dfc5ce6fd4d9102d6582e
safrastyan <safrastyandavidquantum1997@gmail.com>
2019-07-17 01:32:38 +0300

    one LeetCode

---
+++ Competitive Programming/LeetCode/852. Peak Index in a Mountain Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        for (int i = 0; i < A.size(); ++i) {
+            if (A[i] > A[i + 1]) {
+                return i;
+            }
+        }
+        return 10000000;
+    }
+};
