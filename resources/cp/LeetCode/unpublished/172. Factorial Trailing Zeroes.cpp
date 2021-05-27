7d755c55796a9e53b9bb7e806a2917243af6e2a1
Andrey Zakaryan <andreizakaryan@gmail.com>
2021-05-27 23:49:37 +0300

    Leetcode 172 /=

--- resources/cp/LeetCode/unpublished/172. Factorial Trailing Zeroes.cpp
+++ resources/cp/LeetCode/unpublished/172. Factorial Trailing Zeroes.cpp
@@ -3,7 +3,7 @@ public:
     int trailingZeroes(int n) {
         int res = 0;
         while (n > 0) {
-            n = n / 5;
+            n /= 5;
             res += n;
         }
         return res;
