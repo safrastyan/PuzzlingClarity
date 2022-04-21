9526a1ae3ce3decef15002ff457e3e412c97da59
safrastyan <safrastyandavidquantum1997@gmail.com>
2022-04-21 10:21:25 +0100

    basic template

---
+++ resources/cp/snippets/basic_template.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <limits>
+#include <queue>
+#include <map>
+#include <memory>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+
+int main()
+{
+    return 0;
+}
