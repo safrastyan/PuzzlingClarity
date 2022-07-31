2b3f2a38b6d193bf9f154f463132066aef81704a
safrastyan <safrastyandavidquantum1997@gmail.com>
2022-07-31 18:07:28 +0100

    updated cp templates

---
+++ resources/templates/basic_template.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+
+constexpr int NINF = std::numeric_limits<int>::min();
+constexpr int INF = std::numeric_limits<int>::max();
+
+template<typename T>
+T inf();
+template<typename T>
+T ninf();
+template<typename T>
+std::vector<T> reada();
+template<typename T>
+std::vector<T> reada(int n);
+std::vector<int> readai();
+std::vector<int> readai(int n);
+
+
+template <typename T> using vc = std::vector<T>;
+template <typename T> using vvc = std::vector<vc<T>>;
+template <typename T> using vvvc = std::vector<vvc<T>>;
+using ll=long long;
+using ull = unsigned long long;
+
+
+
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+
+	return 0;
+}
+
+
+
+template<typename T>
+inline T inf()
+{
+	return std::numeric_limits<T>::max();
+}
+
+template<typename T>
+inline T ninf()
+{
+	return std::numeric_limits<T>::min();
+}
+
+
+template <typename T>
+inline std::vector<T> reada()
+{
+	int n;
+	std::cin >> n;
+	return reada<T>(n);
+}
+
+
+template<typename T>
+inline std::vector<T> reada(int n)
+{
+	std::vector<int> a(n);
+	for (int i = 0; i < n; ++i) {
+		std::cin >> a[i];
+	}
+	return a;
+}
+
+std::vector<int> readai(int n)
+{
+	return reada<int>(n);
+}
+
+std::vector<int> readai()
+{
+	return reada<int>();
+}
